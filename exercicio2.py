935c15caf3325c238c417b8210ac2c62247b77e7
Mendes_Devv <renanmendescontato3@gmail.com>
2025-06-20 21:56:21 -0300

    Add files via upload

---
+++ exercicio2.py
@@ -0,0 +1,13 @@
+nome = 'Renan Mendes da Silva'
+altura = 1.90
+peso = 97
+imc = peso / altura ** 2
+
+linha_1 = f'{nome} tem {altura:.2f} de altura'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
